bb210f533506193dd5dff6b6a44e76a90154a141
Zuzana Čechová <zu.cechova9@gmail.com>
2024-10-07 19:56:51 +0200

    hotove ukoly

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ document.body.innerHTML += jmeno + ", " + "věk: " + vek
 //vyplata
 const dotazMzda = Number(prompt("Zadejte svojí hodinovou sazbu v korunách",300))
 const mzda = dotazMzda * 8 * 21
-document.body.innerHTML += "Vaše hrubá mzda je " + mzda + "Kč"
+document.body.innerHTML += "Vaše hrubá mzda je " + mzda + "Kč."
+
+//vyplata 2
+const dotazMzda2 = Number(prompt("Zadejte svojí hodinovou sazbu v korunách",300))
+const hodiny = Number(prompt("Zadejte kolik hodin denně pracujete",8))
+const dny = Number(prompt("Zadejte kolik dnů v měsíci pracujete",21))
+document.body.innerHTML += "Vaše hrubá mzda je " + dotazMzda2 * hodiny * dny + "Kč."
