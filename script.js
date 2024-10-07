be0fb24e761f19929b231b3401f5a66567364401
Zuzana Čechová <zu.cechova9@gmail.com>
2024-10-07 19:16:45 +0200

    druhy ukol

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+//ockovani
 const jmeno = prompt("Zadejte své jméno a přijímení", "Jan Novák")
 const vek = Number(prompt("Zadejte svůj věk",28))
 
 document.body.innerHTML += jmeno + ", " + "věk: " + vek
+
+//vyplata
+const dotazMzda = Number(prompt("Zadejte svojí hodinovou sazbu v korunách",300))
+const mzda = dotazMzda * 8 * 21
+document.body.innerHTML += "Vaše hrubá mzda je " + mzda + "Kč"
